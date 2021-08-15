0cb88cd5ea02ac6cb603d5344d278ce72df8385f
Ramveer Shakya <81282262+Ramveer9922@users.noreply.github.com>
2021-08-15 13:25:31 +0530

    Delete Answer1.py

--- Answer1.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-post=requests.get("https://my-json-server.typicode.com/typicode/demo/posts")
-comment=requests.get("https://my-json-server.typicode.com/typicode/demo/comments")
-
-list=[post,comment]
-for i in list:
-    print("Combined data both posts/comments : ",i.text)
